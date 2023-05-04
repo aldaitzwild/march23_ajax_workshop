5efeb6cff349a9fd2c0de579b246022049a1d11c
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-05-04 17:27:58 +0200

    add the correction of workshop

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,12 +5,33 @@ function updateHeadline(title, picture, content) {
 }
 
 document.getElementById('changeHeadlineButton').addEventListener('click', function () {
-    //TODO 1 : Get a random article
+    fetch('api/articles/random')
+    .then(response => response.json())
+    .then(article => updateHeadline(
+        article.title,
+        article.picture,
+        article.content,
+        ))
+    .catch(error => alert(error))
 });
 
 document.getElementById('searchHeadline').addEventListener('input', function(e) {
     //Here we get the value typed in the input
     let search = e.target.value;
 
+    fetch('api/articles/search?search=' + search)
+    .then(response => response.json())
+    .then(articles => {
+        const resultList = document.getElementById('resultList');
+
+        resultList.innerHTML = "";
+
+        for (const article of articles) {
+            resultList.innerHTML += "<li><a href='/article?id=" + article.id + "'>" + article.title + "</a></li>";
+        }
+
+    })
+
+    
     //TODO 2 : Call the route 'api/articles/search' to get the list of the articles targeted by the search
 });
--- src/Model/ArticleManager.php
+++ src/Model/ArticleManager.php
@@ -2,7 +2,27 @@
 
 namespace App\Model;
 
+use PDO;
+
 class ArticleManager extends AbstractManager
 {
-    public const TABLE = 'Article';
+    public const TABLE = 'article';
+
+    public function selectRandomOne()
+    {
+        // prepared request
+        $statement = $this->pdo->query("SELECT * FROM article ORDER BY RAND() LIMIT 1;");
+
+        return $statement->fetch();
+    }
+
+    public function selectArticlesWithKeyword(string $keyword): array
+    {
+        $query = "SELECT * FROM article WHERE title LIKE :keyword;";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue(':keyword', "%$keyword%", PDO::PARAM_STR);
+        $statement->execute();
+
+        return $statement->fetchAll();
+    }
 }
